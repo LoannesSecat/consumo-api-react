fdbd3e3364f86279c31cf5c05d1844a361f68fbf
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2022-05-18 18:45:04 -0500

    Handle fetch API errors 2

--- src/services/Soliciter.jsx
+++ src/services/Soliciter.jsx
@@ -1,9 +1,15 @@
 import { MessageAlert } from "@/actions/ToolActions";
 
 const Soliciter = async (request) => {
-  return fetch(request)
-    .then((res) => res.json())
-    .catch((err) => MessageAlert({ msg: err.message, color: "red" }));
+  if (navigator.onLine) {
+    return fetch(request)
+      .then((res) => res.json())
+      .catch((err) =>
+        MessageAlert({ msg: err.message /* err.stack */, color: "red" })
+      );
+  } else {
+    MessageAlert({ msg: "Sin conexión a internet", color: "red" });
+  }
 };
 
 export default Soliciter;
