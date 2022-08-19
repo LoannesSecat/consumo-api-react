16965387862b51fd993396f502e4c62a09ea88b8
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2022-08-19 18:31:36 -0500

    RequestMode file updated

--- src/services/Soliciter.jsx
+++ src/services/Soliciter.jsx
@@ -1,21 +1,20 @@
 import { MessageAlert } from "../redux/actions/ToolActions";
 import RequestMode from "./RequestMode";
-import useDispatch from "../utils/useDispatch";
+import Dispatch from "../utils/useDispatch";
 
 export default async function Soliciter({ request, mock, action }) {
-  useDispatch({ type: action, payload: "loading" });
+  Dispatch({ type: action, payload: "loading" });
 
-  if (RequestMode === "test") return { type: action, data: mock };
+  if (RequestMode.currentMode() === "test") return { type: action, data: mock };
 
   if (navigator.onLine) {
-    return fetch(request)
-      .then((res) => res.json())
-      .then((json) => {
-        return { type: action, data: json };
-      })
-      .catch((err) => MessageAlert({ msg: err.message, color: "red" }));
+    try {
+      const res = await fetch(request);
+      return res.json().then(el => { return { type: action, data: el } })
+    } catch (err) {
+      MessageAlert({ msg: err.message, color: "red" })
+    }
   }
 
-  if (!navigator.onLine)
-    MessageAlert({ msg: "Sin conexión a internet", color: "red" });
+  if (!navigator.onLine) MessageAlert({ msg: "Sin conexión a internet", color: "red" });
 }
