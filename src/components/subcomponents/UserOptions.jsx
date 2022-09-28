0f4f4ffc595f3047d1c6da18fe34f1c246fe55db
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2022-09-28 15:34:44 -0500

    Dropdown user menu added

--- src/components/subcomponents/UserOptions.jsx
+++ src/components/subcomponents/UserOptions.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import ChevronUp from "~/assets/icons/ChevronUp";
+import Cog8Tooth from "~/assets/icons/Cog8Tooth";
 import userSVG from "~/assets/icons/user.svg";
 import { SignOut } from "~/redux/actions/UserActions";
 import "~/utils/styles/UserOptions.scss";
@@ -7,18 +10,31 @@ import "~/utils/styles/UserOptions.scss";
 export default function UserOpcions() {
   const USER_DATA = useSelector((e) => e.user.userData);
   const navigate = useNavigate();
+  const [classDropdown, setClassDropdown] = useState("dropdown");
+  const BUTTON_SVG = classDropdown === "dropdown" ? <Cog8Tooth /> : <ChevronUp />;
+
+  const PreventDefault = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="user-options">
       {Object.keys(USER_DATA)?.length
         ? (
           <>
-            <div>
+            <div className="user-info">
               <img src={USER_DATA.img ? USER_DATA.img : userSVG} alt="Img" />
               <span>{USER_DATA.nickname}</span>
             </div>
 
-            <button onClick={() => SignOut()}>Salir</button>
+            <div className="options-dropdown">
+              <button onClick={() => { setClassDropdown(classDropdown === "dropdown" ? "dropdown active" : "dropdown"); }}>{BUTTON_SVG}</button>
+              <div className={classDropdown}>
+                <a href="/" onClick={(e) => { PreventDefault(e); }}>Ajustes</a>
+                <a href="/" onClick={(e) => { PreventDefault(e); }}>Favoritos</a>
+                <a href="/" onClick={(e) => { PreventDefault(e); SignOut(); }}>Cerrar sesión</a>
+              </div>
+            </div>
           </>
         )
         : (
