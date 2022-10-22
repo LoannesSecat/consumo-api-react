3fed67232297e9965d47bb95a30a041d8e71e3ec
Loannes <44909579+LoannesSecat@users.noreply.github.com>
2022-10-21 22:15:15 -0500

    GoBackButton fixed for when a new tap is opened

--- src/components/subcomponents/GoBackButton.jsx
+++ src/components/subcomponents/GoBackButton.jsx
@@ -1,10 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "~/utils/styles/GoBackButton.scss";
 
 export default function GoBackButton({ onClick, children }) {
   const navigate = useNavigate();
   const customOnClick = onClick ?? (() => { navigate(-1); });
   const customText = children ?? "Volver";
+  const { key } = useLocation();
 
-  return (<button className="go-back-button" onClick={customOnClick}>{customText}</button>);
+  return key !== "default"
+    ? <button className="go-back-button" onClick={customOnClick}>{customText}</button>
+    : <button className="go-back-button" onClick={() => navigate("/")}>Volver al inicio</button>;
 }
